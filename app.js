aa17d7727f6bda49f6eeee372e9164f6ce456e07
sunny-erickson <sunsites@gmail.com>
2023-01-28 15:49:47 -0800

    java begin

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // grab DOM elements
 
 // set event listeners
+console.log("hello world");
+console.log('42+42');
+console.log(42+42);
+function sayHello(name){
+    console.log(`Hello ${name}`);
+    console.log('Its a beautiful day');
+    console.log('How goes it?');
+}
+sayHello('ryan');
