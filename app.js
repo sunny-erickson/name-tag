ff1e515e273bf9cf1c72543a6f994f67a21d563f
sunny-erickson <sunsites@gmail.com>
2023-01-29 15:10:04 -0800

    3:07 update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // grab DOM elements
-//cleaned up
+//cleaned up This should be different
 const updateBtn = document.getElementById('update');
     updateBtn.addEventListener('click', () => {
 const nameInput = document.getElementById('input');
