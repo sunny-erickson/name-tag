5e20517d060e1c57a4d213f5ca881d8644cdd8bd
sunny-erickson <sunsites@gmail.com>
2023-01-29 15:15:10 -0800

    3:14 update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // grab DOM elements
 //cleaned up This should be different
 const updateBtn = document.getElementById('update');
-    updateBtn.addEventListener('click', () => {
-const nameInput = document.getElementById('input');
+updateBtn.addEventListener('click', () => {
+    const nameInput = document.getElementById('input');
     console.log('I am clicking on the button');
-const nameElem = document.getElementById('name');
+    const nameElem = document.getElementById('name');
       nameElem.textContent=nameInput.value;
 })
