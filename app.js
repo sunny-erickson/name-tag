02bf59cbdfed062371bd2cc47052e1011f244036
sunny-erickson <sunsites@gmail.com>
2023-01-29 14:17:35 -0800

    todays commit 1-29-23 cleaned up 1-29-23

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // grab DOM elements
 
 // set event listeners
-console.log("hello world");
-console.log('42+42');
-console.log(42+42);
 function sayHello(name){
     console.log(`Hello ${name}`);
     console.log('Its a beautiful day');
@@ -12,13 +9,11 @@ function sayHello(name){
 sayHello('ryan');
 
 const updateBtn = document.getElementById('update');
-      updateBtn.addEventListener('click', () => {
+    updateBtn.addEventListener('click', () => {
 const nameInput = document.getElementById('input');
-      console.log('I am clicking on the button');
+    console.log('I am clicking on the button');
 const nameElem = document.getElementById('name');
       nameElem.textContent=nameInput.value;
-
-
 })
 
 
