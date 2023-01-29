c096362340adbdb672e1d776a75b6169f1ea571d
sunny-erickson <sunsites@gmail.com>
2023-01-28 16:53:32 -0800

    Name-Tag Java Deal

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ function sayHello(name){
     console.log('How goes it?');
 }
 sayHello('ryan');
+
+const updateBtn = document.getElementById('update');
+updateBtn.addEventListener('click', () => {
+    const nameInput = document.getElementById('input');
+    console.log('I am clicking on the button');
+const nameElem = document.getElementById('name');
+nameElem.textContent=nameInput.value;
+
+})
+
+
+
+
